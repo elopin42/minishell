631dd1096b0b16bfdbde215edb4e3fe3371d4c69
Ethan Lopin <elopin@bess-f2r3s8.clusters.42paris.fr>
2025-02-24 18:36:25 +0100

    tutututuututut

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,15 +6,30 @@
 /*   By: elopin <elopin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 18:24:18 by elopin            #+#    #+#             */
-/*   Updated: 2025/02/24 18:25:05 by elopin           ###   ########.fr       */
+/*   Updated: 2025/02/24 18:36:05 by elopin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void 	free_exit_touch(t_env *ms)
+{
+	static char ***tab = NULL;
+	int	i;
+
+	i = -1;
+	if (ms)
+		tab = &ms->tokens;
+	else 
+	{
+		while(tab[++i])
+			free(tab[i]);
+		free(tab);
+	}
+}
+
 void	handler(int	sig)
 {
 	(void)	sig;
 	exit(127);
-	// bug si \n a la fin du bye meme si fd a 1
 }
