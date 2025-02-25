495fb3c8a63afecba2b71eb4b20c872618d75082
Ethan Lopin <elopin@bess-f2r6s4.clusters.42paris.fr>
2025-02-25 04:51:22 +0100

    exit_amelioration

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: elopin <elopin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 01:42:38 by elopin            #+#    #+#             */
-/*   Updated: 2025/02/25 04:44:55 by elopin           ###   ########.fr       */
+/*   Updated: 2025/02/25 04:50:27 by elopin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,17 @@
 
 void	choos_command(t_env *ms)
 {
-	int	i;
-
-	i = 0;
 	if (ft_strcmp(ms->tokens[0], "echo") == 0 && ft_strcmp(ms->tokens[1], "-n") == 0)
 		fd_printf(1, "%s", ms->tokens[2]);
 	else if (ft_strcmp(ms->tokens[0], "echo") == 0)
 		fd_printf(1, "%s\n",ms->tokens[1]);
 	else if (ft_strcmp(ms->tokens[0], "exit") == 0)
-		i = 1;
-	free_tab(ms);
-	if (i == 1)
 	{
+		free_tab(ms);
 		fd_printf(1, "exit\n");
 		exit (0);
 	}
+	free_tab(ms);
 }
 
 void 	free_tab(t_env *ms)
