d686f0039fd3701079e95b8bbd4d3943d00a8bc4
Ethan Lopin <elopin@bess-f2r6s4.clusters.42paris.fr>
2025-02-25 03:25:50 +0100

    gestion erreur echo

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: elopin <elopin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 01:42:38 by elopin            #+#    #+#             */
-/*   Updated: 2025/02/25 03:08:08 by elopin           ###   ########.fr       */
+/*   Updated: 2025/02/25 03:25:18 by elopin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 void	choos_command(t_env *ms)
 {
-	if (ft_strcmp(ms->tokens[0], "echo") == 0)
-		fd_printf(1, ms->tokens[1]);
-	else
-		return ;
-	write(1, "\n", 1);
+	if (ft_strcmp(ms->tokens[0], "echo") == 0 && ft_strcmp(ms->tokens[1], "-n") == 0)
+		fd_printf(1, "%s", ms->tokens[2]);
+	else if (ft_strcmp(ms->tokens[0], "echo") == 0)
+		fd_printf(1, "%s\n",ms->tokens[1]);
 }
